4bf1e02dccad33021f6a4ff31252a4e65c16afa3
adimigaker <59336725+adimigaker@users.noreply.github.com>
2026-05-28 17:24:42 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbxNamdlH6zSVDK_-eC6KWYjb6E2Ob3ivI2kcZRD9XkwxKleb0bTZbKT8xXhQPVsgv0P/exec',
+    SITE_NAME: 'Pirate Studio 21',
+    DISQUS_SHORTNAME: 'piratesstudio21',
+    POPULAR_LIMIT: 6,
+    LATEST_LIMIT: 9,
+};
